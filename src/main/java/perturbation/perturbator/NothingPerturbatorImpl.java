0cc7c842d458a3bbd1cad6d0f86b5c37eaa979eb
danglotb <bdanglot@gmail.com>
2016-03-30 15:27:18 +0200

    feature: new Perturbator implementation : Nothing Perturbator

---
+++ src/main/java/perturbation/perturbator/NothingPerturbatorImpl.java
@@ -0,0 +1,46 @@
+package perturbation.perturbator;
+
+/**
+ * Created by spirals on 30/03/16.
+ */
+public class NothingPerturbatorImpl implements Perturbator {
+    @Override
+    public boolean pboolean(boolean value) {
+        return value;
+    }
+
+    @Override
+    public byte pbyte(byte value) {
+        return value;
+    }
+
+    @Override
+    public short pshort(short value) {
+        return value;
+    }
+
+    @Override
+    public int pint(int value) {
+        return value;
+    }
+
+    @Override
+    public long plong(long value) {
+        return value;
+    }
+
+    @Override
+    public char pchar(char value) {
+        return value;
+    }
+
+    @Override
+    public float pfloat(float value) {
+        return value;
+    }
+
+    @Override
+    public double pdouble(double value) {
+        return value;
+    }
+}
