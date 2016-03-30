a9d84f824a1fe156149803b4c4d99e3bc6b9467c
danglotb <bdanglot@gmail.com>
2016-03-30 17:38:39 +0200

    remove useless stuff

--- src/main/java/perturbation/location/PerturbationLocation.java
+++ src/main/java/perturbation/location/PerturbationLocation.java
@@ -2,6 +2,10 @@ package perturbation.location;
 
 import perturbation.perturbator.Perturbator;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by spirals on 30/03/16.
  */
@@ -20,5 +24,23 @@ public interface PerturbationLocation {
     Perturbator getPerturbator();
 
     void setPerturbator(Perturbator pertubator) ;
+
+    static List<PerturbationLocation> getLocationFromClass(Class clazz) {
+
+        Field[] fields = clazz.getFields();
+
+        List<PerturbationLocation> locations = new ArrayList<PerturbationLocation>();
+
+        for (int i = 0 ; i < fields.length ; i++) {
+            if (fields[i].getName().startsWith("__L"))
+                try {
+                    locations.add((PerturbationLocation) fields[i].get(null));
+                } catch (IllegalAccessException e) {//won't occurs any way
+                    e.printStackTrace();
+                }
+        }
+
+        return locations;
+    }
 
 }
--- src/main/java/perturbation/location/PerturbationLocationImpl.java
+++ src/main/java/perturbation/location/PerturbationLocationImpl.java
@@ -3,10 +3,6 @@ package perturbation.location;
 import perturbation.perturbator.InvPerturbatorImpl;
 import perturbation.perturbator.Perturbator;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by spirals on 08/03/16.
  */
@@ -21,8 +17,6 @@ public class PerturbationLocationImpl implements PerturbationLocation {
     private boolean mustBeEnact = false;
 
     private Perturbator perturbator;
-
-    public String replacement;
 
     @Override
     public int getLocationIndex(){
@@ -72,48 +66,12 @@ public class PerturbationLocationImpl implements PerturbationLocation {
 
     @Override
     public String toString() {
-        return locationIndex+"\t"+locationInCode+"\t"+locationType+"\t"+replacement;
-    }
-
-    public static List<PerturbationLocation> getLocationFromClass(Class clazz) {
-
-        Field[] fields = clazz.getFields();
-
-        List<PerturbationLocation> locations = new ArrayList<PerturbationLocation>();
-
-        for (int i = 0 ; i < fields.length ; i++) {
-            if (fields[i].getName().startsWith("__L"))
-                try {
-                    locations.add((PerturbationLocation) fields[i].get(null));
-                } catch (IllegalAccessException e) {//won't occurs any way
-                    e.printStackTrace();
-                }
-        }
-
-        return locations;
+        return locationIndex+"\t"+locationInCode+"\t"+locationType;
     }
 
     @Override
     public boolean equals(Object that) {
         return that instanceof PerturbationLocationImpl && (PerturbationLocationImpl.this.locationIndex) == ((PerturbationLocationImpl)that).locationIndex;
-    }
-
-
-    @Deprecated
-    public int numberOfSuccess = 0;
-    @Deprecated
-    public int numberOfFailure = 0;
-    @Deprecated
-    public int numberOfCall = 0;
-    @Deprecated
-    public int numberOfActivation = 0;
-    @Deprecated
-    public void reset() {
-        this.numberOfFailure = 0;
-        this.numberOfActivation = 0;
-        this.numberOfCall = 0;
-        this.numberOfSuccess = 0;
-        this.replacement = null;
     }
 
 }
