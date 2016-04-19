a231045c305476adea4f88bca1cd94887a5330c5
danglotb <bdanglot@gmail.com>
2016-04-19 15:30:32 +0200

    fix Rename Processor : modify static access

--- src/main/java/processor/RenameProcessor.java
+++ src/main/java/processor/RenameProcessor.java
@@ -1,7 +1,11 @@
 package processor;
 
 import spoon.processing.AbstractProcessor;
-import spoon.reflect.declaration.CtClass;
+import spoon.reflect.code.*;
+import spoon.reflect.declaration.*;
+import spoon.reflect.visitor.filter.TypeFilter;
+import spoon.support.reflect.code.CtTypeAccessImpl;
+
 
 /**
  * Created by spirals on 19/04/16.
@@ -10,7 +14,21 @@ public class RenameProcessor extends AbstractProcessor<CtClass> {
 
     @Override
     public void process(CtClass ctClass) {
+        String oldName = ctClass.getSimpleName();
         ctClass.setSimpleName(ctClass.getSimpleName()+"Instr");
+        CtTypeAccess typeInstrumented = getFactory().Code().createTypeAccess(ctClass.getReference());
+        for (Object field :  ctClass.getElements(new TypeFilter(CtFieldAccess.class))) {
+            if (((CtFieldAccess) field).getVariable().isStatic() &&
+                    ((CtTypeAccessImpl)((CtFieldAccess) field).getTarget()).getAccessedType().getSimpleName().equals(oldName))
+                ((CtFieldAccess)field).setTarget(typeInstrumented);
+        }
+
+        for (Object method : ctClass.getElements(new TypeFilter(CtInvocation.class)))
+            if (((CtInvocation) method).getExecutable().isStatic() &&
+                ((CtInvocation) method).getExecutable().getDeclaringType().getSimpleName().equals(oldName)) {
+            ((CtInvocation) method).setTarget(typeInstrumented);
+        }
+
     }
 
     @Override
