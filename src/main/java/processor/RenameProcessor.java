c2838e1c582190183081da8bb82c293f65bc5fc5
danglotb <bdanglot@gmail.com>
2016-04-19 17:16:02 +0200

    fix RenameProcessor with an uniq clause

--- src/main/java/processor/RenameProcessor.java
+++ src/main/java/processor/RenameProcessor.java
@@ -3,8 +3,11 @@ package processor;
 import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.*;
 import spoon.reflect.declaration.*;
+import spoon.reflect.reference.CtTypeReference;
 import spoon.reflect.visitor.filter.TypeFilter;
 import spoon.support.reflect.code.CtTypeAccessImpl;
+
+import java.util.List;
 
 
 /**
@@ -14,27 +17,19 @@ public class RenameProcessor extends AbstractProcessor<CtClass> {
 
     @Override
     public void process(CtClass ctClass) {
-        String oldName = ctClass.getSimpleName();
-        ctClass.setSimpleName(ctClass.getSimpleName()+"Instr");
-        CtTypeAccess typeInstrumented = getFactory().Code().createTypeAccess(ctClass.getReference());
-        CtThisAccess thisInstrumented = getFactory().Code().createThisAccess(ctClass.getReference());
-        for (Object field :  ctClass.getElements(new TypeFilter(CtFieldAccess.class))) {
-            if (((CtFieldAccess) field).getVariable().isStatic() &&
-                    ((CtTypeAccessImpl)((CtFieldAccess) field).getTarget()).getAccessedType().getSimpleName().equals(oldName))
-                ((CtFieldAccess)field).setTarget(typeInstrumented);
-            if (((CtFieldAccess) field).getTarget() instanceof CtThisAccess)
-                ((CtFieldAccess) field).setTarget(thisInstrumented);
+        String newName = ctClass.getSimpleName() + "Instr";
+        List<CtTypeReference<?>> references = ctClass.getElements(new TypeFilter<CtTypeReference<?>>(CtTypeReference.class) {
+            @Override
+            public boolean matches(CtTypeReference<?> element) {
+                return super.matches(element) && element.getQualifiedName().equals(ctClass.getQualifiedName());
+            }
+        });
+
+        for (CtTypeReference<?> reference : references) {
+            reference.setSimpleName(newName);
         }
 
-        for (Object method : ctClass.getElements(new TypeFilter(CtInvocation.class))) {
-            if (((CtInvocation)method).getExecutable().getDeclaringType() == null)
-                continue;
-            if (((CtInvocation) method).getExecutable().isStatic() && ((CtInvocation) method).getExecutable().getDeclaringType().getSimpleName().equals(oldName))
-                ((CtInvocation) method).setTarget(typeInstrumented);
-            if (((CtInvocation) method).getTarget() instanceof CtThisAccess)
-                   ((CtInvocation) method).setTarget(thisInstrumented);
-        }
-
+        ctClass.setSimpleName(newName);
     }
 
     @Override
