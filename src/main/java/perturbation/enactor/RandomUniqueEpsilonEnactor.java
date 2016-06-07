692f1b5c0eb3008471ae2be9194cd35f667bfaa2
bdanglot <bdanglot@gmail.com>
2016-06-07 14:40:11 +0200

    feat: new enactor

---
+++ src/main/java/perturbation/enactor/RandomUniqueEpsilonEnactor.java
@@ -0,0 +1,37 @@
+package perturbation.enactor;
+
+/**
+ * Created by bdanglot on 07/06/16.
+ */
+public class RandomUniqueEpsilonEnactor extends RandomEnactorImpl {
+
+    private boolean firstTime;
+
+    public RandomUniqueEpsilonEnactor() {
+        super();
+        this.firstTime = true;
+    }
+
+    public RandomUniqueEpsilonEnactor(float epsilon) {
+        super(epsilon);
+        this.firstTime = true;
+    }
+
+    public RandomUniqueEpsilonEnactor(int seed, float epsilon) {
+        super(seed, epsilon);
+        this.firstTime = true;
+    }
+
+
+    @Override
+    public boolean shouldBeActivated() {
+        boolean activation = super.shouldBeActivated() && this.firstTime;
+        this.firstTime = !activation;
+        return activation;
+    }
+
+    @Override
+    public String toString() {
+        return "RNDU";
+    }
+}
