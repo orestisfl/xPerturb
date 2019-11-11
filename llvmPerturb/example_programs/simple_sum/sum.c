01189b94746d904ceb5b4f61e376fc82a31370ec
Alexander Koski <akoski@kth.se>
2019-11-11 21:34:56 +0100

    Small program to test perturbations on

--- llvmPerturb/example_programs/simple_sum/sum.c
+++ llvmPerturb/example_programs/simple_sum/sum.c
@@ -1,48 +1,44 @@
 #include <stdio.h>
-
 #include <stdlib.h>
 #include <stdint.h>
-// int add(int num1, int num2)
-// {
-//      int sum;
-//      sum = num1+num2;
-//      return sum;
-// }
 
-
-int intis(int num1, int num2)
+int add(int num1, int num2)
 {
      int sum;
      sum = num1+num2;
      return sum;
 }
 
-char mul(char num1, char num2)
-{
-     char sum;
-     sum = num1+num2;
-     return sum;
-}
 // int sub(int num1, int num2)
 // {
 //      int sum;
 //      sum = num1-num2;
 //      return sum;
 // }
+
+// char mul(char num1, char num2)
+// {
+//      char sum;
+//      sum = num1*num2;
+//      return sum;
+// }
+
 // int div(int num1, int num2)
 // {
 //      int sum;
 //      sum = num1/num2;
 //      return sum;
 // }
+
 int main()
 {
      char var1, var2;
      var1 = 2;
      var2 = 3;
-
-     char res = mul(var1, var2);
+     char res = add(var1, var2);
+     // char res = sub(var1, var2);
+     // char res = mul(var1, var2);
+     // char res = div(var1, var2);
      printf ("%d\n", res);
-
      return 0;
 }
