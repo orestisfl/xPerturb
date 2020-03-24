1a74558492d82be895decb65175e4b69d4fa60e6
Orestis Floros <forestis@kth.se>
2020-03-24 12:36:45 +0100

    WIP: TRACERWASM

--- llvmPerturb/attacks/targets.py
+++ llvmPerturb/attacks/targets.py
@@ -1,7 +1,11 @@
+import multiprocessing
 import os
+import random
+import re
+import tempfile
 from subprocess import Popen, PIPE
 
-from deadpool_dca import ARCH, TracerGrind, bin2daredevil
+from deadpool_dca import ARCH, Tracer, TracerGrind, bin2daredevil, DefaultFilters
 
 
 class TraceChess2016:
@@ -13,11 +17,11 @@ class TraceChess2016:
     @staticmethod
     def processinput(iblock, blocksize):
         p = "%0*x" % (2 * blocksize, iblock)
-        return None, [p[j * 2 : (j + 1) * 2] for j in range(len(p) / 2)]
+        return None, [p[j * 2: (j + 1) * 2] for j in range(len(p) / 2)]
 
     @staticmethod
     def processoutput(output, blocksize):
-        return int("".join([x for x in output.split("\n") if x.find("OUTPUT") == 0][0][10:].split(" ")), 16,)
+        return int("".join([x for x in output.split("\n") if x.find("OUTPUT") == 0][0][10:].split(" ")), 16, )
 
     def accuireTrace(self, ppr=None, ppo=None):
         if ppr and ppo:
@@ -41,7 +45,7 @@ class TraceChess2016:
         bin2daredevil(
             configs={
                 "attack_sbox": {"algorithm": "AES", "position": "LUT/AES_AFTER_SBOX"},
-                "attack_multinv": {"algorithm": "AES", "position": "LUT/AES_AFTER_MULTINV",},
+                "attack_multinv": {"algorithm": "AES", "position": "LUT/AES_AFTER_MULTINV", },
             }
         )
 
@@ -186,3 +190,147 @@ class TraceKryptologik:
         sp = Popen(["time", "daredevil", "-c", f], stdout=PIPE, stderr=PIPE,)
         out, err = sp.communicate()
         return out, err
+
+
+WASM_TRACE = ["node", "--experimental-wasm-bigint", "$(which wasm-trace)", "--optimize=0"]
+
+
+class TracerWASM(Tracer):
+    def __init__(
+        self,
+        target,
+        processinput=TraceChess2016.processinput,
+        processoutput=TraceChess2016.processoutput,
+        arch=ARCH.amd64,
+        blocksize=16,
+        tmptracefile="default",
+        addr_range="default",
+        stack_range="default",
+        filters=(DefaultFilters.mem_addr1_rw1,),
+        debug=True,
+    ):
+        super(TracerWASM, self).__init__(
+            os.path.abspath(target),
+            processinput,
+            processoutput,
+            arch,
+            blocksize,
+            tmptracefile,
+            addr_range,
+            stack_range,
+            filters,
+            tolerate_error=False,
+            shell=True,
+            debug=debug,
+        )
+        # TODO
+        if addr_range == "default":
+            self.addr_range = "0x400000-0x3ffffff"
+        if stack_range == "default":
+            if self.arch == ARCH.i386:
+                self.stack_range = (0xF0000000, 0xFFFFFFFF)
+            if self.arch == ARCH.amd64:
+                self.stack_range = (0xFF0000000, 0xFFFFFFFFF)
+
+        # Instrument target WebAssembly file.
+        instrumented = self.target[0] + "-instrumented"
+        if not os.path.exists(instrumented):
+            # However, only do this if the instrumented target does not exist
+            self._exec(WASM_TRACE + ["-M", "-o", "/dev/null", "--save-wasm", instrumented, self.target[0]], None)
+        assert os.path.exists(instrumented), "Instrumented file successfully created"
+        self.target[0] = instrumented
+
+        self.verbose = False
+
+    def get_trace(self, n, iblock, output):
+        oblock = self.processoutput(output, self.blocksize)
+        trace_log = "./trace%d-%d-converted.log" % (n, iblock)
+
+        self._trace_init(n, iblock, oblock)
+        with open(trace_log, "r") as trace:
+            for line in iter(trace.readline, ""):
+                mem_mode = line[line.index("MODE") + 6]
+                mem_addr = int(line[line.index("START_ADDRESS") + 15 : line.index("START_ADDRESS") + 31], 16,)
+                mem_size = int(line[line.index("LENGTH") + 7 : line.index("LENGTH") + 10])
+                mem_data = int(line[line.index("DATA") + 6 :].replace(" ", ""), 16)
+                for f in self.filters:
+                    if mem_mode in f.modes and f.condition(self.stack_range, mem_addr, mem_size, mem_data):
+                        self._trace_data[f.keyword].append(f.extract(mem_addr, mem_size, mem_data))
+        self._trace_dump()
+        return oblock
+
+    def run(self, n, **_):
+        pool = multiprocessing.Pool(multiprocessing.cpu_count())
+        args = []
+        for i in range(n):
+            iblock = random.randint(0, (1 << (8 * self.blocksize)) - 1)
+            input_stdin, input_args = self.processinput(iblock, self.blocksize)
+            if input_stdin is None:
+                input_stdin = b""
+            if input_args is None:
+                input_args = []
+            args.append((i, iblock, self.target + input_args, input_stdin))
+        # oblock=self.get_trace(i, iblock)
+        # TODO: get_trace
+        return args, pool.map(_exec_wasm_trace, args)
+
+
+def _exec_wasm_trace(args):
+    """Easily parallelizable function that executes wasm-trace"""
+    i, iblock, cmd, input_stdin = args
+
+    # wasm-trace uses file .wasm-trace.csv on the current work directory.
+    # Because of this, we need to create a new empty directory to avoid each process conflicting with the others.
+    # The trace.log output should always be in the original work directory.
+    trace_log = os.path.abspath("./trace%d-%d.log" % (i, iblock))
+    wd = tempfile.mkdtemp(suffix="-%d" % iblock)
+
+    # When wasm-trace is called without optimization arguments it just runs the instrumented file,
+    # providing the needed missing functions
+    cmd = ("cd %s && " % wd) + " ".join(WASM_TRACE + ["-o", trace_log] + cmd) + (" && rm -rf %s" % wd)
+
+    print(cmd, wd)
+    p = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE, shell=True, executable="/bin/bash")
+    out, err = p.communicate(input=input_stdin)
+    if p.returncode != 0:
+        raise RuntimeError("Cmd '%s' exited with code %d, message:\n%s" % (cmd, p.returncode, err))
+
+    with open(trace_log) as f1:
+        trace_log_converted = trace_log[:-4] + "-converted.log"
+        with open(trace_log_converted, "w") as f2:
+            f2.write("\n".join(wasm_trace2tracergrind(f1)))
+    return out
+
+
+def wasm_trace2tracergrind(f):
+    def hexpad(d, length=8):
+        fmt = "{:0>%d}" % (length * 2)
+        formatted = fmt.format(hex(d)[2:])
+        if formatted.endswith("L"):
+            return formatted[:-1]
+        return formatted
+
+    pattern = re.compile(r"\s*(\d+) \| (i32|i64) \| (store|load) \s* (\d+)\+(\d+) (\d) (\-?\d+)")
+    for line in f:
+        line = line.strip()
+        if not line:
+            continue
+
+        exec_id, _, mode, address, offset, length, value = pattern.search(line).groups()
+        length = int(length)
+        mode = "W" if mode == "store" else "R"
+        # addresses should have 16 hex digits
+        ins_address = hexpad(0)  # We don't care
+        start_address = hexpad(int(address) + int(offset))
+        value = int(value)
+        if value < 0:
+            value += 2 ** (length * 8)
+        value = hexpad(value, length)
+        yield "[M] EXEC_ID: %s INS_ADDRESS: %s START_ADDRESS: %s LENGTH: %d MODE: %s DATA: %s" % (
+            exec_id,
+            ins_address,
+            start_address,
+            length,
+            mode,
+            value,
+        )
